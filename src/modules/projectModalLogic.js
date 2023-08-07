c229c9ce698a7e78a2446914fe27f5c354918723
baldx <mr.ahmad.m2006@gmail.com>
2023-08-07 21:22:20 +0200

    add delete button logic & rules

--- src/modules/projectModalLogic.js
+++ src/modules/projectModalLogic.js
@@ -13,13 +13,21 @@ class project {
 const submitBtn = document.querySelector(".submitProject");
 const container = document.querySelector(".popUpProject");
 const titleInput = document.querySelector("#titleProject");
+const deleteBtn = document.querySelector(".delete-project");
 
 let logic = () => {
     submitBtn.addEventListener("click", element => {
-    addProjectToLibrary();
-    element.preventDefault();
+        if (rules()) {
+            addProjectToLibrary();
+            element.preventDefault();
+        }
+        else {
+            element.preventDefault();
+            alert("Enter a Title!")
+        }
+    
  })
-}   
+}
 
 function addProjectToLibrary() {
     container.style.display = "none";
@@ -29,11 +37,21 @@ function addProjectToLibrary() {
     resetForm()
 }
 
+deleteBtn.addEventListener("click", () => {
+    deleteBtn.parentElement.remove()
+    projectLibrary.pop()
+})
+
+function rules() {
+    if (titleInput.value == "") return false;
+    else return true;
+}
+
 function resetForm() {
     titleInput.value = ""
 }
 
-function addToList() {
+function addToList(array) {
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
     const btn = document.createElement("button");
@@ -46,6 +64,11 @@ function addToList() {
     deleteBtn.classList.add("delete-project")
     deleteBtn.innerHTML = "🗑️"
     deleteBtn.style.margin = "8px"
+
+    deleteBtn.addEventListener("click", () => {
+        deleteBtn.parentElement.remove();
+        projectLibrary.splice(projectLibrary.indexOf(array), 1)
+    })
 }
 
 export default logic();
