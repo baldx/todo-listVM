cbf178337ca26480b78f694469ed66e174583210
baldx <mr.ahmad.m2006@gmail.com>
2023-08-13 23:08:59 +0200

    remove skeleton code

--- src/index.js
+++ src/index.js
@@ -16,67 +16,48 @@ addTaskBtn.addEventListener("click", () => {
 })
 
 
-const priorityBtn = document.querySelector(".priority");
-priorityBtn.innerHTML = localStorage.getItem("priority");
+// const priorityBtn = document.querySelector(".priority");
+// priorityBtn.innerHTML = localStorage.getItem("priority");
 
-if (priorityBtn.innerHTML == "") priorityBtn.innerHTML = "High";
-
-
-priorityBtn.addEventListener("click", () => {
-    if (priorityBtn.innerHTML == "High") {
-        priorityBtn.classList.remove("High");
-        priorityBtn.classList.add("Low");
-        priorityBtn.innerHTML = "Low";
-    }
-    else if (priorityBtn.innerHTML == "Low") {
-        priorityBtn.classList.remove("Low");
-        priorityBtn.classList.add("Medium");
-        priorityBtn.innerHTML = "Medium";
-    }
-    else if (priorityBtn.innerHTML == "Medium") {
-        priorityBtn.classList.remove("Medium");
-        priorityBtn.classList.add("High");
-        priorityBtn.innerHTML = "High";
-    }
-
-    localStorage.setItem("priority", priorityBtn.innerHTML)
-})
-
-const status = document.querySelector(".check");
-
-window.onload = () => {
-   if (status.innerHTML == "Done") {
-    status.classList.add("Done");
-    status.innerHTML = "Done";
-    } 
-    else if (status.innerHTML == "Undone") {
-        status.classList.add("UnDone")
-        status.innerHTML = "Undone"
-    }
-
-    if (priorityBtn.innerHTML == "High") priorityBtn.classList.add("High") 
-    else if (priorityBtn.innerHTML == "Medium") priorityBtn.classList.add("Medium")
-    else if (priorityBtn.innerHTML == "Low") priorityBtn.classList.add("Low")
-}
+// if (priorityBtn.innerHTML == "") priorityBtn.innerHTML = "High";
 
 
+// priorityBtn.addEventListener("click", () => {
+//     if (priorityBtn.innerHTML == "High") {
+//         priorityBtn.classList.remove("High");
+//         priorityBtn.classList.add("Low");
+//         priorityBtn.innerHTML = "Low";
+//     }
+//     else if (priorityBtn.innerHTML == "Low") {
+//         priorityBtn.classList.remove("Low");
+//         priorityBtn.classList.add("Medium");
+//         priorityBtn.innerHTML = "Medium";
+//     }
+//     else if (priorityBtn.innerHTML == "Medium") {
+//         priorityBtn.classList.remove("Medium");
+//         priorityBtn.classList.add("High");
+//         priorityBtn.innerHTML = "High";
+//     }
 
-status.innerHTML = localStorage.getItem("status");
+//     localStorage.setItem("priority", priorityBtn.innerHTML)
+// })
 
-if (status.innerHTML == "") status.innerHTML = "Undone"
+// const status = document.querySelector(".check");
+
+// window.onload = () => {
+//    if (status.innerHTML == "Done") {
+//     status.classList.add("Done");
+//     status.innerHTML = "Done";
+//     } 
+//     else if (status.innerHTML == "Undone") {
+//         status.classList.add("UnDone")
+//         status.innerHTML = "Undone"
+//     }
+
+//     if (priorityBtn.innerHTML == "High") priorityBtn.classList.add("High") 
+//     else if (priorityBtn.innerHTML == "Medium") priorityBtn.classList.add("Medium")
+//     else if (priorityBtn.innerHTML == "Low") priorityBtn.classList.add("Low")
+// }
 
 
-status.addEventListener("click", () => {
-    if (status.innerHTML == "Undone") {
-        status.classList.remove("UnDone");
-        status.classList.add("Done");
-        status.innerHTML = "Done";
-    }
-    else if (status.innerHTML == "Done") {
-        status.classList.remove("Done");
-        status.classList.add("UnDone");
-        status.innerHTML = "Undone";
-    }
 
-    localStorage.setItem("status", status.innerHTML);
-})
